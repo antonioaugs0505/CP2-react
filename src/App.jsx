d9aa8f0b4d0f1f635f4abb57de2fc797fd2b6856
antonioaugs0505 <rm550344@fiap.com.br>
2023-09-06 10:27:58 -0300

    Criando Menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Menu from './components/Menu'
 
 function App() {
  
 
   return (
     <>
+    <Menu/>
     <Outlet/>
     </>
   )
