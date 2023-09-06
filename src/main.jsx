79deb30809e6d2bbabc314659edb765df900aa23
antonioaugs0505 <rm550344@fiap.com.br>
2023-09-06 10:15:10 -0300

    Fazendo os imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProduto from './routes/EditarProduto/index.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
